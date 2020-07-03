88014b8a32768af0e5bce04283bcb23d10e4b799
LotusterHaar <lotus.terhaar@orange-juice.nl>
2020-07-03 11:23:10 +0200

    Puppeteer is implemented for the Orange Juice stage-formulier

--- ojform.js
+++ ojform.js
@@ -10,7 +10,7 @@ var $ = jQuery = require('jquery')(window);
 
 //Defining chromeOptions for defining path, time to execute and setting headless mode to false
 const chromeOptions = {
-    slowMo: 10,
+    slowMo: 100,
     headless:false,
     args: ['--start-maximized']
 };
@@ -23,89 +23,70 @@ const chromeOptions = {
     //Navigating to Test Project Contact Form
     await page.goto('https://www.orange-juice.nl/stage');
 
-    var HTML = await page.content()
-
-    //console.log(HTML.$( "label:contains('Naam')" ));
-
-    //a[contains(text(), 'Some text')]
-
-    //
-    // await page.waitForXPath('//label[contains(text(), "Naam")]', 5000);
-    // const elementHandles = await page.$x('//label[contains(text(), "Naam")]');
-    //
-    // // const elementHandles = await page.$x( "//label[contains(text(), 'Naam')]");
-    //
-    // for (const element of elementHandles) {
-    //     const idLabel = await page.evaluate(el => el, element);
-    //     console.log("idLabel")
-    //     console.log(idLabel)
-    // }
-
-    // export const selectOption = async ( label, value ) => {
-    //     const [ selectEl ] = await page.$x( `//label[@class="label"][contains(text(),"${ label }")]/following-sibling::select[@class="components-select-control__input"]` );
-    //     const selectId = await page.evaluate(
-    //         ( el ) => el.id,
-    //         selectEl
-    //     );
-    //     await page.select( `#${ selectId }`, value );
-    // };
-
-    // let id = await page.evaluate(el => el, elHandle[0]);
-    // console.log(id);
-    // // const elHandle = await page.$( "label:contains('Naam')")
-    //     // console.log(elHandle);
-
-    // var xpath = "//label[text()='SearchingText']";
-    // var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    // console.log(matchingElement);
-    // const className = await elHandle.getProperty('className')
-
-    //console.log(className);
-    // prepare to get the textContent of the selector above (use page.evaluate)
-    //let lamudiNewPropertyCount = await page.evaluate(el => console.log(el), elHandle[0]);
-
     //Taking screenshot of initial page UI
     await page.screenshot({path: 'beforefill.png'});
-
 
     //Waiting for form to load
     await page.waitFor('.form');
 
-    // await page.evaluate(label => {
-    //     $(`input:contains('${label}')`)[0].type('Lotus')
-    // }, 'Naam')
-
-    //
-    // await page.waitForXPath("//*[@class='form-group' and contains(text(),'Naam')]", 5000);
-    // const [example] = await page.$x("//*[@class='form-group' and contains(text(),'Naam')]");
-    // console.log(example)
-    // await example.type('Lotus');
-
     //Waiting for field First Name to appear and storing its UI element in name variable and entering test value
-    const fname = await page.waitForSelector("#name");
-    await fname.type("Testing UI Automation");
+    const nameEl = await page.$("input[name='name']")
+    nameEl.type('abc');
+
+    // //Waiting for field Email to appear and storing its UI element in name variable and entering test value
+    const emailEl = await page.waitForSelector("#email");
+    await emailEl.type("lotus@orange-juice.nl");
+
+    // Search for the radio buttons by their identifier, click on them (the last value of a form-group will remain selected)
+
+    const radioValues2 = await page.$$eval(
+        'input[type="radio"]',
+        radios =>
+            radios.map(radio => {
+                radio.click();
+                return radio.checked
+            })
+    );
+    console.log(radioValues2);
+
+    const radioValues1 = await page.$$eval(
+        'input[type="radio"]',
+        radios =>
+            radios.map(radio => {
+                return radio.checked
+            })
+    );
+    console.log(radioValues1);
 
 
-    //Waiting for field Email to appear and storing its UI element in name variable and entering test value
-    const email = await page.waitForSelector("#email");
-    await email.type("test@testmail.com");
+    const dateEl = await page.waitForSelector("#date");
+    await dateEl.type('31082020')
 
-    // //Waiting for dropdown element and clicking on it
-    // const dropdown = "#select2-subject-container";
-    // await page.waitForSelector(dropdown);
-    // await page.click(dropdown);
-    //
-    // //Enter on subject
-    // await page.keyboard.press('Enter');
+
+
+    // get the selector input type=file (for upload file)
+    await page.waitForSelector('input[type=file]');
+    await page.waitFor(1000);
+
+    // get the ElementHandle of the selector above
+    const inputUploadHandle = await page.$('input[type=file]');
+
+    // prepare file to upload, I'm using test_to_upload.jpg file on same directory as this script
+    // Photo by Ave Calvar Martinez from Pexels https://www.pexels.com/photo/lighthouse-3361704/
+    let fileToUpload = 'testcv.docx';
+
+    // Sets the value of the file input to fileToUpload
+    inputUploadHandle.uploadFile(fileToUpload);
+
 
 
     //Entering Message in the message field
     const message = await page.waitForSelector("#motivation");
     await message.type("This is a test message created by automated testing of UI using Puppeteer");
 
-    //Clicking on Contact us
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Enter');
+    //Clicking on Send, to submit thge form
+    const sendButtonNodeSelector = ".btn-primary";
+    await page.click(sendButtonNodeSelector);
 
     //Take screenshot of filled form UI
     await page.screenshot({path: 'afterfill.png'});
